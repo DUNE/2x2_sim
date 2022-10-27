485c51810cf7f3b1575204d3220d29b37c90dc94
Matt Kramer <matt314159@gmail.com>
2022-10-27 11:03:59 -0700

    Rename 2x2_beam_cherry.mac -> 2x2_beam.mac
    Since there's nothing in the macro that's specific to cherrypicking

--- run-edep-sim/submit_example.sh
+++ run-edep-sim/submit_example.sh
@@ -3,7 +3,7 @@
 export ARCUBE_CHERRYPICK=1
 export ARCUBE_DET_LOCATION=ProtoDUNE-ND
 export ARCUBE_DK2NU_DIR=/global/cfs/cdirs/dune/users/2x2EventGeneration/NuMI_dk2nu/newtarget-200kA_20220409
-export ARCUBE_EDEP_MAC=macros/2x2_beam_cherry.mac
+export ARCUBE_EDEP_MAC=macros/2x2_beam.mac
 # export ARCUBE_EXPOSURE=5E17
 export ARCUBE_EXPOSURE=1E17
 export ARCUBE_GEOM=geometry/Merged2x2MINERvA_noRock.gdml
