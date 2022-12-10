a1d998a5ecd341f73bda31ff39aa438b7c29ecd3
Matt Kramer <matt314159@gmail.com>
2022-12-09 21:56:06 -0800

    delete these maybe

--- fw_test/arcube_tasks/edep_sim_task.py
+++ fw_test/arcube_tasks/edep_sim_task.py
@@ -12,6 +12,7 @@ class EdepSimTask(FiretaskBase):
         env = os.environ | {k: str(v) for k, v in fw_spec.items()
                             if k.startswith('ARCUBE_')}
 
+        # container digest 1079ee4e88
         shifter = 'shifter --image=docker:wilkinsonnu/nuisance_project:2x2_sim_prod'
         call(f'{shifter} -- ./fw_edep_sim.sh', shell=True,
              cwd=fw_spec['ARCUBE_BASE'], env=env)
