3e4fcf53459f3e2edf7107e5fbe595d58392c089
Matt Kramer <matt314159@gmail.com>
2023-01-05 17:35:41 -0800

    initial track reader

---
+++ muongun/DrawTracks.C
@@ -0,0 +1,84 @@
+using namespace ROOT;
+
+// RDataFrame/RVec don't support multidimensional C arrays
+// (e.g. StdHepX4), so must use TTreeReader instead
+void DrawGenieTracksRVec(const char* gfile="gntp.0.gtrac.root")
+{
+  RDataFrame df("gRooTracker", gfile);
+
+  bool doPrint = true;
+
+  auto DrawEvent = [&doPrint](int StdHepN, RVecI StdHepPdg, RVecI StdHepStatus,
+                              RVecD StdHepX4, RVecD StdHepP4)
+                      // RVec<RVecD> StdHepX4, RVec<RVecD> StdHepP4)
+  {
+    if (! doPrint)
+      return;
+
+    cout << StdHepPdg.size() << endl;
+    cout << StdHepX4.size() << endl;
+    cout << StdHepP4.size() << endl;
+
+    for (int i = 0; i < StdHepX4.size(); ++i)
+      cout << StdHepX4[i] << endl;
+
+    for (int i = 0; i < StdHepP4.size(); ++i)
+      cout << StdHepP4[i] << endl;
+
+    // for (int i = 0; i < StdHepN; ++i) {
+    //   if (abs(StdHepPdg[i]) == 13 && StdHepStatus[i] == 1) {
+    //     // double* x4 = &StdHepX4[4*i];
+    //     // double* p4 = &StdHepP4[4*i];
+
+    //     double x4[4] = {StdHepX4[4*i], StdHepX4[4*i + 1], StdHepX4[4*i + 2], StdHepX4[4*i + 3]};
+    //     double p4[4] = {StdHepP4[4*i], StdHepP4[4*i + 1], StdHepP4[4*i + 2], StdHepP4[4*i + 3]};
+
+    //     cout << x4[0] << " " << x4[1] << " " << x4[2] << " " << x4[3] << endl;
+    //     cout << p4[0] << " " << p4[1] << " " << p4[2] << " " << p4[3] << endl;
+    //     cout << endl;
+    //   }
+    // }
+
+    doPrint = false;
+  };
+
+  df.Foreach(DrawEvent,
+             {"StdHepN", "StdHepPdg", "StdHepStatus",
+              "StdHepX4", "StdHepP4"});
+}
+
+
+void DrawGenieTracks(const char* gfile="gntp.0.gtrac.root")
+{
+  TTreeReader r("gRooTracker", new TFile(gfile));
+  TTreeReaderValue<Int_t> StdHepN(r, "StdHepN");
+  TTreeReaderArray<Int_t> StdHepPdg(r, "StdHepPdg");
+  TTreeReaderArray<Int_t> StdHepStatus(r, "StdHepStatus");
+  TTreeReaderArray<Double_t> StdHepX4(r, "StdHepX4");
+  TTreeReaderArray<Double_t> StdHepP4(r, "StdHepP4");
+
+  // r.Next();
+  // cout << StdHepX4.GetSize() << endl;
+
+  // for (int i = 0; i < 4*StdHepX4.GetSize(); ++i) {
+  //   cout << StdHepX4[i] << endl;
+  // }
+
+  // for (int i = 0; i < 4*StdHepP4.GetSize(); ++i) {
+  //   cout << StdHepP4[i] << endl;
+  // }
+
+  while (r.Next()) {
+    for (int i = 0; i < *StdHepN; ++i) {
+      if (abs(StdHepPdg[i]) == 13 && StdHepStatus[i] == 1) {
+        double* x4 = &StdHepX4[4*i];
+        double* p4 = &StdHepP4[4*i];
+
+        cout << x4[0] << " " << x4[1] << " " << x4[2] << " " << x4[3] << endl;
+        cout << p4[0] << " " << p4[1] << " " << p4[2] << " " << p4[3] << endl;
+        cout << endl;
+      }
+    }
+    // return;
+  }
+}
---
+++ muongun/LoadStuff.C
@@ -0,0 +1,5 @@
+{
+    gROOT->ProcessLine(".L ../run-edep-sim/geometry/inspectgeom.C");
+    gROOT->ProcessLine(".L ../run-edep-sim/geometry/eve_display.C");
+    gROOT->ProcessLine(".L DrawTracks.C");
+}
