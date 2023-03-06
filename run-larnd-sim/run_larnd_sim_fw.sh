f0eb6105c59e66ffcfb85f527e8d2128ccfb836a
Matt Kramer <matt314159@gmail.com>
2023-03-06 14:51:16 -0800

    Initial run_larnd_sim_fw.sh
    This is actually broken since it's trying to use the ROOT edep files as
    input, d'oh.

---
+++ run-larnd-sim/run_larnd_sim_fw.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+if [[ "$NERSC_HOST" == "cori" ]]; then
+    export HDF5_USE_FILE_LOCKING=FALSE
+fi
+
+# source venv/bin/activate
+source load_larnd_sim.sh
+
+globalIdx=$ARCUBE_INDEX
+echo "globalIdx is $globalIdx"
+
+dk2nuAll=("$ARCUBE_DK2NU_DIR"/*.dk2nu)
+dk2nuCount=${#dk2nuAll[@]}
+dk2nuIdx=$((globalIdx % dk2nuCount))
+dk2nuFile=${dk2nuAll[$dk2nuIdx]}
+echo "dk2nuIdx is $dk2nuIdx"
+echo "dk2nuFile is $dk2nuFile"
+
+outDir=$PWD/output/$ARCUBE_OUT_NAME
+mkdir -p $outDir
+
+outName=$(basename "$dk2nuFile" .dk2nu).$(printf "%03d" $((globalIdx / dk2nuCount)))
+echo "outName is $outName"
+
+timeFile=$outDir/TIMING/$outName.time
+mkdir -p "$(dirname "$timeFile")"
+timeProg=/usr/bin/time
+
+run() {
+    echo RUNNING "$@"
+    time "$timeProg" --append -f "$1 %P %M %E" -o "$timeFile" "$@"
+}
+
+inFile=$PWD/../run-spill-build/output/${ARCUBE_SPILL_NAME}/EDEPSIM_SPILLS/${outName}.EDEPSIM_SPILLS.root
+
+larndOutDir=$outDir/LARNDSIM
+mkdir -p $larndOutDir
+
+outFile=$larndOutDir/${outName}.LARNDSIM.h5
+
+run simulate_pixels_spills.py --input_filename "$inFile" \
+    --output_filename "$outFile" \
+    --detector_properties larnd-sim/larndsim/detector_properties/2x2.yaml \
+    --pixel_layout larnd-sim/larndsim/pixel_layouts/multi_tile_layout-2.3.16.yaml \
+    --response_file larnd-sim/larndsim/bin/response_44.npy \
+    --light_lut_filename larnd-sim/larndsim/bin/lightLUT.npz \
+    --light_det_noise_filename larnd-sim/larndsim/bin/light_noise-2x2-example.npy
