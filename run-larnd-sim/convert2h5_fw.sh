087e8700885a7dd4a579d29cfb20dabcab13f1b9
Matt Kramer <matt314159@gmail.com>
2023-03-06 19:18:19 -0800

    Add convert2h5_fw.sh

---
+++ run-larnd-sim/convert2h5_fw.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+image=docker:wilkinsonnu/nuisance_project:2x2_sim_prod
+if [[ "$SHIFTER_IMAGEREQUEST" != "$image" ]]; then
+    shifter --image=$image --module=none -- "$0" "$@"
+    exit
+fi
+
+source /environment             # provided by the container
+
+# in the container, install numpy fire h5py tqdm
+source convert.venv/bin/activate
+
+if [[ "$NERSC_HOST" == "cori" ]]; then
+    export HDF5_USE_FILE_LOCKING=FALSE
+fi
+
+globalIdx=$ARCUBE_INDEX
+echo "globalIdx is $globalIdx"
+
+dk2nuAll=("$ARCUBE_DK2NU_DIR"/*.dk2nu)
+dk2nuCount=${#dk2nuAll[@]}
+dk2nuIdx=$((globalIdx % dk2nuCount))
+dk2nuFile=${dk2nuAll[$dk2nuIdx]}
+echo "dk2nuIdx is $dk2nuIdx"
+echo "dk2nuFile is $dk2nuFile"
+
+outDir=$PWD/output/$ARCUBE_OUT_NAME
+mkdir -p $outDir
+
+outName=$(basename "$dk2nuFile" .dk2nu).$(printf "%03d" $((globalIdx / dk2nuCount)))
+echo "outName is $outName"
+
+timeFile=$outDir/TIMING/$outName.time
+mkdir -p "$(dirname "$timeFile")"
+timeProg=$PWD/../run-edep-sim/tmp_bin/time      # container is missing /usr/bin/time
+
+run() {
+    echo RUNNING "$@"
+    time "$timeProg" --append -f "$1 %P %M %E" -o "$timeFile" "$@"
+}
+
+inFile=$PWD/../run-spill-build/output/${ARCUBE_SPILL_NAME}/EDEPSIM_SPILLS/${outName}.EDEPSIM_SPILLS.root
+
+h5OutDir=$outDir/EDEPSIM_H5
+mkdir -p $h5OutDir
+
+outFile=$h5OutDir/${outName}.EDEPSIM.h5
+rm -f $outFile
+
+run ./convert_edepsim_roottoh5.py --input_files $inFile --output_file $outFile
