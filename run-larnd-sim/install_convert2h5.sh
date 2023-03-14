6af40a52a6e20de2bd8066d4b1b4af8e5b171ba8
Matt Kramer <matt314159@gmail.com>
2023-03-14 10:27:05 -0700

    Save results of "pip freeze" in run-larnd-sim
    This should allow for reproducibly setting up the two venvs (for
    larnd-sim and convert2h5).

---
+++ run-larnd-sim/install_convert2h5.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [[ -z "$ARCUBE_CONTAINER" ]]; then
+    echo "Set \$ARCUBE_CONTAINER to the GENIE+edep-sim container"
+    exit 1
+fi
+
+# Reload in Shifter
+if [[ "$SHIFTER_IMAGEREQUEST" != "$ARCUBE_CONTAINER" ]]; then
+    shifter --image=$ARCUBE_CONTAINER --module=none -- "$0" "$@"
+    exit
+fi
+
+source /environment
+
+rm -rf convert.venv
+python3 -m venv convert.venv
+source convert.venv/bin/activate
+
+pip3 install -r pip/requirements.convert2h5.sh
--- run-larnd-sim/install_larnd_sim.sh
+++ run-larnd-sim/install_larnd_sim.sh
@@ -8,6 +8,14 @@ rm -rf larnd-sim venv
 python -m venv venv/
 source venv/bin/activate
 
+# Might need to remove larnd-sim from this requirements file
+pip install -r pip/requirements.larnd-sim.txt
+exit
+
+# If installation via requirements.txt doesn't work, the below should rebuild
+# the venv. Ideally, install everything *except* larnd-sim using the
+# requirements.txt, then just use the block at the bottom to install larnd-sim.
+
 pip install -U pip wheel setuptools
 
 # https://docs.nersc.gov/development/languages/python/using-python-perlmutter/#installing-with-pip
@@ -21,9 +29,3 @@ pip install -U pip wheel setuptools
   sed 's/cupy/cupy-cuda11x/' setup.py.orig > setup.py
   pip install .
   mv setup.py.orig setup.py )
-
-# The job needs dotlockfile
-( git clone https://github.com/miquels/liblockfile.git
-  cd liblockfile || exit
-  ./configure
-  make -j4 )
