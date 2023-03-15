07360682b3ec652ffbecd23f3ace24a3b8793190
Matt Kramer <matt314159@gmail.com>
2023-03-14 21:50:28 -0700

    Add (instructive) install_spill_build.sh

---
+++ run-spill-build/install_spill_build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "See libTG4Event/MAKEP, modify it as necessary, and run it inside the container"
+
+# NOTE: See makeLibTG4Event.sh to see how libTG4Event was generated. To
+# regenerate the source (which should not be necessary), an arbitrary edep-sim
+# output file is required.
