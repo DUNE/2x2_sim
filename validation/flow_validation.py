0eeee5378a337689a78b6a4150155e972fd726e1
Kevin Wood <kwood@lbl.gov>
2023-03-15 15:11:37 -0700

    updates to packet dataset validation, including a version for flow data sets which will eventually look at other datasets as well

--- validation/flow_validation.py
+++ validation/flow_validation.py
@@ -7,24 +7,24 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 SPILL_PERIOD = 1.2e7 # units = ticks
 
-def main(sim_file):
+def main(flow_file):
 
-    sim_h5 = h5py.File(sim_file,'r')
+    flow_h5 = h5py.File(flow_file,'r')
     print('\n----------------- File content -----------------')
-    print('File:',sim_file)
-    print('Keys in file:',list(sim_h5.keys()))
-    for key in sim_h5.keys():
-        print('Number of',key,'entries in file:', len(sim_h5[key]))
+    print('File:',flow_file)
+    print('Keys in file:',list(flow_h5.keys()))
+    for key in flow_h5.keys():
+        print('Number of',key,'entries in file:', len(flow_h5[key]))
     print('------------------------------------------------\n')
 
-    output_pdf_name = sim_file.split('.h5')[0]+'_validations.pdf'
+    output_pdf_name = flow_file.split('.h5')[0]+'_validations.pdf'
     # temperarily, put output in this directory, not the same as the
     # simulation file itself
     output_pdf_name = output_pdf_name.split('/')[-1] # !!
     with PdfPages(output_pdf_name) as output:
 
         # get the packet data and create some masks:
-        packets = sim_h5['packets']
+        packets = flow_h5['/charge/packets/data']
         packet_index = np.array(list(range(0,len(packets))))
         data_packet_mask = packets['packet_type'] == 0
 
@@ -68,7 +68,7 @@ def main(sim_file):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--sim_file', default=None, type=str,help='''string corresponding to the path of the larnd-sim output simulation file to be considered''')
+    parser.add_argument('--flow_file', default=None, type=str,help='''string corresponding to the path of the ndlar_flow output file to be considered''')
     args = parser.parse_args()
     main(**vars(args))
 
