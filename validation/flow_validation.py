7296e015c2e848a1b43016c366435942b4923bea
Kevin Wood <kwood@lbl.gov>
2023-03-15 16:00:04 -0700

    adds simple event display to flow validation script

--- validation/flow_validation.py
+++ validation/flow_validation.py
@@ -28,6 +28,60 @@ def main(flow_file):
         packet_index = np.array(list(range(0,len(packets))))
         data_packet_mask = packets['packet_type'] == 0
 
+        ### Event display
+        fig = plt.figure(layout="constrained")
+        gs = fig.add_gridspec(5,3,height_ratios=[1,3,1,3,1])
+        ax1 = fig.add_subplot(gs[1,0])
+        ax2 = fig.add_subplot(gs[1,1])
+        ax3 = fig.add_subplot(gs[1,2])
+        ax4 = fig.add_subplot(gs[3,0])
+        ax5 = fig.add_subplot(gs[3,1])
+        ax6 = fig.add_subplot(gs[3,2])
+        
+        ax1.hist2d(flow_h5['/charge/calib_prompt_hits/data']['z'],flow_h5['/charge/calib_prompt_hits/data']['y'],weights=flow_h5['/charge/calib_prompt_hits/data']['E'],bins=(300,300))
+        ax4.scatter(flow_h5['/charge/calib_prompt_hits/data']['z'],flow_h5['/charge/calib_prompt_hits/data']['y'],s=0.5,alpha=0.1)
+        ax1.set_xlabel(r'z [mm]')
+        ax1.set_ylabel(r'y [mm]')
+        ax4.set_xlabel(r'z [mm]')
+        ax4.set_ylabel(r'y [mm]')
+        ax2.hist2d(flow_h5['/charge/calib_prompt_hits/data']['x'],flow_h5['/charge/calib_prompt_hits/data']['z'],weights=flow_h5['/charge/calib_prompt_hits/data']['E'],bins=(300,300))
+        ax5.scatter(flow_h5['/charge/calib_prompt_hits/data']['x'],flow_h5['/charge/calib_prompt_hits/data']['z'],s=0.5,alpha=0.1)
+        ax2.set_xlabel(r'x [mm]')
+        ax2.set_ylabel(r'z [mm]')
+        ax5.set_xlabel(r'x [mm]')
+        ax5.set_ylabel(r'z [mm]')
+        ax3.hist2d(flow_h5['/charge/calib_prompt_hits/data']['x'],flow_h5['/charge/calib_prompt_hits/data']['y'],weights=flow_h5['/charge/calib_prompt_hits/data']['E'],bins=(300,300))
+        ax6.scatter(flow_h5['/charge/calib_prompt_hits/data']['x'],flow_h5['/charge/calib_prompt_hits/data']['y'],s=0.5,alpha=0.1)
+        ax3.set_xlabel(r'x [mm]')
+        ax3.set_ylabel(r'y [mm]')
+        ax6.set_xlabel(r'x [mm]')
+        ax6.set_ylabel(r'y [mm]')
+        output.savefig()
+        plt.close()
+
+        ### Hit level 1D position distributions
+        plt.hist(flow_h5['/charge/calib_prompt_hits/data']['z'],bins=300)
+        plt.xlabel('z [cm]')
+        plt.yscale('log')
+        plt.ylim([1,2e3])
+        output.savefig()
+        plt.close()
+
+        plt.show()
+        plt.hist(flow_h5['/charge/calib_prompt_hits/data']['y'],bins=300)
+        plt.xlabel('y [cm]')
+        plt.yscale('log')
+        plt.ylim([1,2e3])
+        output.savefig()
+        plt.close()
+
+        plt.hist(flow_h5['/charge/calib_prompt_hits/data']['x'],bins=300)
+        plt.xlabel('x [cm]')
+        plt.yscale('log')
+        plt.ylim([1,2e3])
+        output.savefig()
+        plt.close()
+
         ### Plot time structure of packets: 
         plt.plot(packets['timestamp'][data_packet_mask],packet_index[data_packet_mask],'o',label='data packets',linestyle='None')
         plt.plot(packets['timestamp'][~data_packet_mask],packet_index[~data_packet_mask],'o',label='other',linestyle='None')
@@ -64,7 +118,6 @@ def main(flow_file):
         plt.legend(ncol=4,bbox_to_anchor=(-0.05,1.00),loc='lower left')
         output.savefig()
         plt.close()
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
