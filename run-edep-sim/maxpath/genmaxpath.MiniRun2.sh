672d8e2502e4032eea86f71a791e24fea00f5e0b
Matt Kramer <matt314159@gmail.com>
2023-04-03 15:20:03 -0700

    Update container in gemaxpath.MiniRun2.sh
    It doesn't actually make any difference, since only edep-sim, not GENIE,
    has changed. But might as well keep things up-to-date. Should probably
    have a less obscure place where we reference the container, but for
    better or for worse, that information currently "lives" in
    fireworks4dune.

--- run-edep-sim/maxpath/genmaxpath.MiniRun2.sh
+++ run-edep-sim/maxpath/genmaxpath.MiniRun2.sh
@@ -2,7 +2,7 @@
 
 # run me from run-edep-sim
 
-export ARCUBE_CONTAINER=mjkramer/sim2x2:genie_edep.LFG_testing.20230228
+export ARCUBE_CONTAINER=mjkramer/sim2x2:genie_edep.LFG_testing.20230228.v2
 tune=D22_22a_02_11b
 
 ./GENIE_max_path_length_gen.sh geometry/Merged2x2MINERvA_v2/Merged2x2MINERvA_v2_withRock.gdml $tune
