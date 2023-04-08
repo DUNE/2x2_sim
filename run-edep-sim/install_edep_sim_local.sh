713ac08c04afc1f7f6c4b791d8250a1de143fd44
Matt Kramer <matt314159@gmail.com>
2023-04-07 20:31:40 -0700

    initial working(?) build of Kazu's edep-sim

---
+++ run-edep-sim/install_edep_sim_local.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+ARCUBE_CONTAINER=mjkramer/sim2x2:genie_edep.LFG_testing.20230228.v2
+
+# Reload in Shifter if necessary
+if [[ "$SHIFTER_IMAGEREQUEST" != "$ARCUBE_CONTAINER" ]]; then
+    shifter --image=$ARCUBE_CONTAINER --module=none -- "$0" "$@"
+    exit
+fi
+
+source environment_local.sh
+
+rm -rf local
+mkdir -p local/install
+cd local
+
+git clone https://github.com/jbeder/yaml-cpp.git
+mkdir yaml-cpp/build
+pushd yaml-cpp/build
+cmake -DCMAKE_INSTALL_PREFIX=../../install ..
+make -j16 && make install
+popd
+
+git clone https://github.com/DeepLearnPhysics/DLPGenerator
+pushd DLPGenerator
+source setup.sh
+make -j16
+
+git clone -b feat-mpvmpr https://github.com/drinkingkazu/edep-sim.git
+pushd edep-sim/build
+cmake -DCMAKE_INSTALL_PREFIX=../../install ..
+make -j16
+make install
