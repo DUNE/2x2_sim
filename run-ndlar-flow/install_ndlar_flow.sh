a93ae697ddc62b784ab4c6679895405a7d6ae85b
Matt Kramer <matt314159@gmail.com>
2023-04-11 20:39:42 -0700

    Update install_ndlar_flow.sh, add static files

--- run-ndlar-flow/install_ndlar_flow.sh
+++ run-ndlar-flow/install_ndlar_flow.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 
+set -o errexit
+
+module load python
+
+python -m venv flow.venv
+source flow.venv/bin/activate
+
 # install h5flow
-git clone git@github.com:peter-madigan/h5flow.git
+git clone https://github.com/larpix/h5flow.git
 cd h5flow
+git checkout d2864e84d1bcd2553139373d2f39baab7a9bdfdf
 pip install .
 cd ..
 
 # install ndlar_flow
-git clone git@github.com:larpix/ndlar_flow.git
+git clone https://github.com/larpix/ndlar_flow.git
 cd ndlar_flow
+git checkout 946336a19343bcd9a7ef5b4ef56a0c8be13c2fff
 pip install .
 cd ..
+
+datadir=ndlar_flow/data/proto_nd_flow
+
+cp static/multi_tile_layout-2.3.16.yaml $datadir
+cp static/2x2.yaml $datadir
+cp static/runlist-2x2-mcexample.txt $datadir
+cp static/light_module_desc-0.0.0.yaml $datadir
