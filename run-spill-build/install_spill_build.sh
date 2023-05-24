4cffc05baffd4bbd7463d68ce934579a4ccf6af8
Andrew Cudd <Andrew.Cudd@colorado.edu>
2023-05-24 10:48:18 -0600

    Finished (maybe) modification to install scripts

--- run-spill-build/install_spill_build.sh
+++ run-spill-build/install_spill_build.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 
-# Reload in Shifter if necessary
-if [[ "$SHIFTER_IMAGEREQUEST" != "$ARCUBE_CONTAINER" ]]; then
-    shifter --image=$ARCUBE_CONTAINER --module=none -- "$0" "$@"
+if [[ -z "$ARCUBE_CONTAINER" ]]; then
+    echo "Set \$ARCUBE_CONTAINER to the GENIE+edep-sim container"
+    exit 1
+fi
+
+if [[ "$ARCUBE_RUNTIME" == "SHIFTER" ]]; then
+    # Reload in Shifter
+    if [[ "$SHIFTER_IMAGEREQUEST" != "$ARCUBE_CONTAINER" ]]; then
+        shifter --image=$ARCUBE_CONTAINER --module=none -- "$0" "$@"
+        exit
+    fi
+
+elif [[ "$ARCUBE_RUNTIME" == "SINGULARITY" ]]; then
+    # Or reload in Singularity
+    if [[ "$SINGULARITY_NAME" != "$ARCUBE_CONTAINER" ]]; then
+        singularity exec -B $ARCUBE_DIR $ARCUBE_CONTAINER_DIR/$ARCUBE_CONTAINER /bin/bash "$0" "$@"
+        exit
+    fi
+
+else
+    echo "Unsupported \$ARCUBE_RUNTIME"
     exit
 fi
 
