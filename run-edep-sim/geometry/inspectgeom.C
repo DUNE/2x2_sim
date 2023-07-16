3452752b7fef0475c2589a6c24a8dddfde485509
Matt Kramer <mjkramer@users.noreply.github.com>
2023-07-15 23:13:24 -0700

    Add GetGlobalPos to inspectgeom.C

--- run-edep-sim/geometry/inspectgeom.C
+++ run-edep-sim/geometry/inspectgeom.C
@@ -56,10 +56,51 @@ void RecursiveTransparency(TGeoVolume *vol, Int_t transp) {
   }
 }
 
+typedef std::vector<TGeoNode*> GeoPath;
 
+std::vector<GeoPath> FindNodes(TGeoNode* base_node, const char* regexp)
 {
+  TPRegexp pregexp(regexp);
 
+  const auto inner = [&](const auto& self,
+                         TGeoNode* node,
+                         const GeoPath& path2parent)
+    -> std::vector<GeoPath> {
+    std::vector<GeoPath> result;
+    GeoPath path2me = path2parent;
+    path2me.push_back(node);
+    // check this node
+    if (pregexp.MatchB(node->GetName()))
+      result.push_back(path2me);
+    // check its daughters
+    for (size_t i = 0; i < node->GetNdaughters(); ++i) {
+      TGeoNode* daughter = node->GetDaughter(i);
+      std::vector<GeoPath> paths = self(self, daughter, path2me);
+      for (const GeoPath& p : paths)
+        result.push_back(p);
+    }
+    return result;
+  };
+
+  return inner(inner, base_node, {});
 }
 
+std::array<double, 3> GetGlobalPos(const char* regexp)
 {
+  TGeoNode* root_node = gGeoManager->GetNode(0);
+  std::vector<GeoPath> paths = FindNodes(root_node, regexp);
+  assert(paths.size() == 1);
+  const GeoPath& path = paths[0];
+
+  double local[3] = {0., 0., 0.};
+  double master[3];
+  for (int i = path.size()-1; i >= 0; --i) {
+    TGeoNode* node = path[i];
+    TGeoMatrix* mat = node->GetMatrix();
+    mat->LocalToMaster(local, master);
+    std::copy(master, master+3, local);
+  }
+
+  std::array<double, 3> result = {master[0], master[1], master[2]};
+  return result;
 }
