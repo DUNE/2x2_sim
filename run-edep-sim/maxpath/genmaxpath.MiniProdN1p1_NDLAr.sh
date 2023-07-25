1ddbee1b38b1d1819de00d7d4f52af33637ff5dd
Alexander Booth <abooth@login11.chn>
2023-07-25 08:34:35 -0700

    Add a note about location of maxpath file to generation script.

--- run-edep-sim/maxpath/genmaxpath.MiniProdN1p1_NDLAr.sh
+++ run-edep-sim/maxpath/genmaxpath.MiniProdN1p1_NDLAr.sh
@@ -5,4 +5,5 @@
 export ARCUBE_CONTAINER='mjkramer/sim2x2:genie_edep.3_04_00.20230620'
 tune=AR23_20i_00_000
 
+# Output file has been moved to /global/cfs/cdirs/dune/users/abooth/maxpath/nd_hall_with_lar_tms_sand.AR23_20i_00_000.maxpath.xml.
 ./GENIE_max_path_length_gen.sh /dvs_ro/cfs/cdirs/dune/users/abooth/gdml/nd_hall_with_lar_tms_sand.gdml $tune
