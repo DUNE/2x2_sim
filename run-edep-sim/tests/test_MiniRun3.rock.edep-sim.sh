4d8e09764b0b6ecca6c122bdd59019addd9a7dff
Matt Kramer <mjkramer@users.noreply.github.com>
2023-09-12 11:03:09 -0700

    Fix indices for MiniRun3 test

--- run-edep-sim/tests/test_MiniRun3.rock.edep-sim.sh
+++ run-edep-sim/tests/test_MiniRun3.rock.edep-sim.sh
@@ -13,7 +13,7 @@ export ARCUBE_TUNE='D22_22a_02_11b'
 export ARCUBE_XSEC_FILE='/global/cfs/cdirs/dune/users/2x2EventGeneration/inputs/NuMI/D22_22a_02_11b.all.LFG_testing.20230228.spline.xml'
 export ARCUBE_OUT_NAME='test_MiniRun3.rock'
 
-for i in $(seq 10); do
+for i in $(seq 0 9); do
     ARCUBE_INDEX=$i ./run_edep_sim.sh &
 done
 
