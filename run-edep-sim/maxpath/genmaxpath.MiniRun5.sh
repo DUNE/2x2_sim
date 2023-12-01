553c7e61ecd39422d5bfe6e32baf8e1187d5e02a
Alejandro Diaz <diaza@caltech.edu>
2023-11-30 19:17:08 -0800

    Add new geometry info into GNuMIFlux and maxpath files

---
+++ run-edep-sim/maxpath/genmaxpath.MiniRun5.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# run me from run-edep-sim
+
+export ARCUBE_CONTAINER=mjkramer/sim2x2:genie_edep.3_04_00.20230912
+tune=AR23_20i_00_000
+basegeom=geometry/Merged2x2MINERvA_v4/Merged2x2MINERvA_v4
+
+./GENIE_max_path_length_gen.sh ${basegeom}_withRock.gdml $tune
+./GENIE_max_path_length_gen.sh ${basegeom}_noRock.gdml $tune
+./GENIE_max_path_length_gen.sh ${basegeom}_justRock.gdml $tune
