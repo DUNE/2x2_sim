1e27a3e0ca2042ef23cdb1b288ee3ea5a13b51ab
Matt Kramer <mjkramer@users.noreply.github.com>
2023-12-29 11:04:26 -0800

    Add ARCUBE_LARNDSIM_CONFIG

--- run-larnd-sim/run_larnd_sim.sh
+++ run-larnd-sim/run_larnd_sim.sh
@@ -32,19 +32,26 @@ rm -f "$outFile"
 
 cd "$ARCUBE_INSTALL_DIR"
 
-[ -z "$ARCUBE_LARNDSIM_DETECTOR_PROPERTIES" ] && export ARCUBE_LARNDSIM_DETECTOR_PROPERTIES="larnd-sim/larndsim/detector_properties/2x2.yaml"
-[ -z "$ARCUBE_LARNDSIM_PIXEL_LAYOUT" ] && export ARCUBE_LARNDSIM_PIXEL_LAYOUT="larnd-sim/larndsim/pixel_layouts/multi_tile_layout-2.4.16.yaml"
-[ -z "$ARCUBE_LARNDSIM_RESPONSE_FILE" ] && export ARCUBE_LARNDSIM_RESPONSE_FILE="larnd-sim/larndsim/bin/response_44.npy"
-[ -z "$ARCUBE_LARNDSIM_LUT_FILENAME" ] && export ARCUBE_LARNDSIM_LUT_FILENAME="/global/cfs/cdirs/dune/www/data/2x2/simulation/larndsim_data/light_LUT_M123_v1/lightLUT_M123.npz"
-[ -z "$ARCUBE_LARNDSIM_LIGHT_DET_NOISE_FILENAME" ] && export ARCUBE_LARNDSIM_LIGHT_DET_NOISE_FILENAME="larnd-sim/larndsim/bin/light_noise_2x2_4mod_July2023.npy"
-[ -z "$ARCUBE_LARNDSIM_SIMULATION_PROPERTIES" ] && export ARCUBE_LARNDSIM_SIMULATION_PROPERTIES="larnd-sim/larndsim/simulation_properties/2x2_NuMI_sim.yaml"
+if [[ -n "$ARCUBE_LARNDSIM_CONFIG" ]]; then
+    run simulate_pixels.py "$ARCUBE_LARNDSIM_CONFIG" \
+        --input_filename "$inFile" \
+        --output_filename "$outFile" \
+        --rand_seed "$seed"
+else
+    [ -z "$ARCUBE_LARNDSIM_DETECTOR_PROPERTIES" ] && export ARCUBE_LARNDSIM_DETECTOR_PROPERTIES="larnd-sim/larndsim/detector_properties/2x2.yaml"
+    [ -z "$ARCUBE_LARNDSIM_PIXEL_LAYOUT" ] && export ARCUBE_LARNDSIM_PIXEL_LAYOUT="larnd-sim/larndsim/pixel_layouts/multi_tile_layout-2.4.16.yaml"
+    [ -z "$ARCUBE_LARNDSIM_RESPONSE_FILE" ] && export ARCUBE_LARNDSIM_RESPONSE_FILE="larnd-sim/larndsim/bin/response_44.npy"
+    [ -z "$ARCUBE_LARNDSIM_LUT_FILENAME" ] && export ARCUBE_LARNDSIM_LUT_FILENAME="/global/cfs/cdirs/dune/www/data/2x2/simulation/larndsim_data/light_LUT_M123_v1/lightLUT_M123.npz"
+    [ -z "$ARCUBE_LARNDSIM_LIGHT_DET_NOISE_FILENAME" ] && export ARCUBE_LARNDSIM_LIGHT_DET_NOISE_FILENAME="larnd-sim/larndsim/bin/light_noise_2x2_4mod_July2023.npy"
+    [ -z "$ARCUBE_LARNDSIM_SIMULATION_PROPERTIES" ] && export ARCUBE_LARNDSIM_SIMULATION_PROPERTIES="larnd-sim/larndsim/simulation_properties/2x2_NuMI_sim.yaml"
 
-run simulate_pixels.py --input_filename "$inFile" \
-    --output_filename "$outFile" \
-    --detector_properties "$ARCUBE_LARNDSIM_DETECTOR_PROPERTIES" \
-    --pixel_layout "$ARCUBE_LARNDSIM_PIXEL_LAYOUT" \
-    --response_file "$ARCUBE_LARNDSIM_RESPONSE_FILE" \
-    --light_lut_filename  "$ARCUBE_LARNDSIM_LUT_FILENAME" \
-    --light_det_noise_filename "$ARCUBE_LARNDSIM_LIGHT_DET_NOISE_FILENAME" \
-    --rand_seed $seed \
-    --simulation_properties "$ARCUBE_LARNDSIM_SIMULATION_PROPERTIES"
+    run simulate_pixels.py --input_filename "$inFile" \
+        --output_filename "$outFile" \
+        --detector_properties "$ARCUBE_LARNDSIM_DETECTOR_PROPERTIES" \
+        --pixel_layout "$ARCUBE_LARNDSIM_PIXEL_LAYOUT" \
+        --response_file "$ARCUBE_LARNDSIM_RESPONSE_FILE" \
+        --light_lut_filename  "$ARCUBE_LARNDSIM_LUT_FILENAME" \
+        --light_det_noise_filename "$ARCUBE_LARNDSIM_LIGHT_DET_NOISE_FILENAME" \
+        --rand_seed $seed \
+        --simulation_properties "$ARCUBE_LARNDSIM_SIMULATION_PROPERTIES"
+fi
