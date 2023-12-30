38823b66e4e6e01e74aa891d5e93c88944a1db47
Matt Kramer <mjkramer@users.noreply.github.com>
2023-12-29 17:14:37 -0800

    Update install_everything.sh

--- admin/install_everything.sh
+++ admin/install_everything.sh
@@ -2,26 +2,29 @@
 
 # Run me from the root directory of 2x2_sim
 
+# Currently larnd-sim and ndlar_flow are the only things we're installing
+# locally. Everything else comes either from a container or CVMFS.
+
 set -o errexit
 
 # This is the "default" container. It can be overridden by exporting
 # ARCUBE_CONTAINER before running e.g. run_edep_sim.sh
-export ARCUBE_RUNTIME="SHIFTER"
-export ARCUBE_CONTAINER=mjkramer/sim2x2:genie_edep.3_04_00.20230620
+# export ARCUBE_RUNTIME="SHIFTER"
+# export ARCUBE_CONTAINER=mjkramer/sim2x2:genie_edep.3_04_00.20230620
 
 # export ARCUBE_RUNTIME="SINGULARITY"
 # export ARCUBE_CONTAINER=sim2x2_genie_edep.LFG_testing.20230228.v2.sif
 
-export ARCUBE_DIR=$PWD
-export ARCUBE_CONTAINER_DIR=$ARCUBE_DIR/admin/containers
+# export ARCUBE_DIR=$PWD
+# export ARCUBE_CONTAINER_DIR=$ARCUBE_DIR/admin/containers
 
-pushd run-spill-build
-./install_spill_build.sh
-popd
+# pushd run-spill-build
+# ./install_spill_build.sh
+# popd
 
-pushd run-convert2h5
-./install_convert2h5.sh
-popd
+# pushd run-convert2h5
+# ./install_convert2h5.sh
+# popd
 
 pushd run-larnd-sim
 ./install_larnd_sim.sh
@@ -31,6 +34,6 @@ pushd run-ndlar-flow
 ./install_ndlar_flow.sh
 popd
 
-pushd validation
-./install_validation.sh
-popd
+# pushd validation
+# ./install_validation.sh
+# popd
