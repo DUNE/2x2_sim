8f3ce7c2928bac7fc6a765264eead27ca4a5af7e
Matt Kramer <mjkramer@users.noreply.github.com>
2024-01-22 13:08:49 -0800

    Update install_mlreco.sh

--- run-mlreco/install_mlreco.sh
+++ run-mlreco/install_mlreco.sh
@@ -3,7 +3,9 @@
 export ARCUBE_RUNTIME=SHIFTER
 # export ARCUBE_CONTAINER=deeplearnphysics/larcv2:ub20.04-cuda11.3-cudnn8-pytorch1.10.0-larndsim-2022-11-03
 ## Above but with venv support:
-export ARCUBE_CONTAINER=mjkramer/sim2x2:mlreco001
+# export ARCUBE_CONTAINER=mjkramer/sim2x2:mlreco001
+# This is the one that Francois has actually been using:
+export ARCUBE_CONTAINER=deeplearnphysics/larcv2:ub20.04-cuda11.6-pytorch1.13-larndsim
 
 source ../util/reload_in_container.inc.sh
 
@@ -12,16 +14,30 @@ set -o errexit
 # recommended by Jeremy
 unset which
 
+mkdir weights
+cd weights
+wget https://portal.nersc.gov/project/dune/data/2x2/simulation/mlreco_data/weights/snapshot-249.ckpt
+cd ..
+
 mkdir install
 cd install
 
-python3 -m venv --system-site-packages mlreco.venv
+# The Ubuntu container doesn't support Python's built-in "venv" module unless we
+# globally "apt install python3.8-venv". To avoid the pain of modifying the
+# container, instead let's use virtualenv.
+wget https://bootstrap.pypa.io/virtualenv/3.8/virtualenv.pyz
+python virtualenv.pyz --system-site-packages mlreco.venv
+rm virtualenv.pyz
+
+# python3 -m venv --system-site-packages mlreco.venv
 source mlreco.venv/bin/activate
-pip3 install --upgrade pip setuptools wheel
+pip install --upgrade pip setuptools wheel
 # pip install 'ruamel.yaml<0.18.0' # for deprecated load()
-pip install scikit-build # for SuperaAtomic
-pip install scikit-learn # for flow2supera
-pip install --upgrade torch_geometric # for mlreco
+
+## Need the following?
+# pip install scikit-build # for SuperaAtomic
+# pip install scikit-learn # for flow2supera
+# pip install --upgrade torch_geometric # for mlreco
 
 git clone -b v2_2_0 https://github.com/DeepLearnPhysics/larcv2.git
 cd larcv2
@@ -50,7 +66,7 @@ cd h5flow
 pip install .
 cd ..
 
-git clone -b v1.1.0 https://github.com/sindhu-ku/flow2supera.git
+git clone -b v1.1.1 https://github.com/sindhu-ku/flow2supera.git
 cd flow2supera
 pip install .
 cd ..
