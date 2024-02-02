eed5f9bf7017e91ade9da1def719ee31f8dda62d
Matt Kramer <mjkramer@users.noreply.github.com>
2024-02-01 17:27:56 -0800

    Bump flow2supera

--- run-mlreco/install_mlreco.sh
+++ run-mlreco/install_mlreco.sh
@@ -67,7 +67,7 @@ pip install .
 cd ..
 
 # git clone -b v1.1.1 https://github.com/sindhu-ku/flow2supera.git
-git clone -b MiniRun4.5_beta1 https://github.com/mjkramer/flow2supera.git
+git clone -b MiniRun4.5_beta2 https://github.com/mjkramer/flow2supera.git
 ## Don't pip install because e.g. config files are expected to live near
 ## __file__
 # cd flow2supera
