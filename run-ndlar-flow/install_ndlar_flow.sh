b27370dfd5adb5c59e88d53203aada4bd0089fb6
Matt Kramer <mjkramer@users.noreply.github.com>
2024-04-03 12:12:27 -0700

    ndlar_flow: Pin pyyaml-include
    2.0 introduced changes that break h5flow

--- run-ndlar-flow/install_ndlar_flow.sh
+++ run-ndlar-flow/install_ndlar_flow.sh
@@ -34,6 +34,8 @@ python -m venv flow.venv
 source flow.venv/bin/activate
 pip install --upgrade pip setuptools wheel
 
+pip install pyyaml-include==1.3.2
+
 # install h5flow
 git clone -b main https://github.com/larpix/h5flow.git
 cd h5flow
