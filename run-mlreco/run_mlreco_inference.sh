d6989f9bcf97d3e1181268a5a7472aea5d120348
noe_roy <roy@lal.in2p3.fr>
2024-05-01 22:29:07 -0400

    Addapted the run files to use the new configs

--- run-mlreco/run_mlreco_inference.sh
+++ run-mlreco/run_mlreco_inference.sh
@@ -8,7 +8,7 @@ source load_mlreco.inc.sh
 outFile=${tmpOutDir}/${outName}.MLRECO_INF.hdf5
 inName=${ARCUBE_IN_NAME}.${globalIdx}
 inFile=${ARCUBE_OUTDIR_BASE}/run-mlreco/${ARCUBE_IN_NAME}/LARCV/${subDir}/${inName}.LARCV.root
-config=2x2_full_chain_230907_trigger_tmp.cfg
+config=2x2_full_chain_240403.cfg
 
 tmpDir=$(mktemp -d)
 mkdir "${tmpDir}/log_trash" "${tmpDir}/weight_trash"
