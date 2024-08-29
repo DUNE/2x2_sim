8588d59b9989dad526c7b7cebb142cce1b0c7879
Alexander Booth <abooth@login19.chn.perlmutter.nersc.gov>
2024-08-29 09:39:33 -0700

    Add distinction between compulsory and non-compulsory arguments in run_cafmaker.
    (cherry picked from commit 4e241a575efbe7b3e59e694c408b690330f9b837)

--- run-cafmaker/run_cafmaker.sh
+++ run-cafmaker/run_cafmaker.sh
@@ -13,15 +13,22 @@ outFile=${tmpOutDir}/${outName}.CAF.root
 flatOutFile=${tmpOutDir}/${outName}.CAF.flat.root
 cfgFile=$(mktemp --suffix .cfg)
 
-./gen_cafmaker_cfg.py \
+# Compulsory arguments.
+args_gen_cafmaker_cfg=( \
     --base-dir "$ARCUBE_OUTDIR_BASE" \
-    --ghep-nu-name "$ARCUBE_GHEP_NU_NAME" \
-    --ghep-rock-name "$ARCUBE_GHEP_ROCK_NAME" \
     --mlreco-name "$ARCUBE_MLRECO_NAME" \
-    --minerva-name "$ARCUBE_MINERVA_NAME" \
     --caf-path "$outFile" \
     --cfg-file "$cfgFile" \
-    --file-id "$ARCUBE_INDEX"
+    --file-id "$ARCUBE_INDEX" \
+    )
+
+[ -n "${ARCUBE_GHEP_NU_NAME}" ] && args_gevgen_fnal+=( --ghep-nu-name "$ARCUBE_GHEP_NU_NAME" )
+[ -n "${ARCUBE_GHEP_ROCK_NAME}" ] && args_gevgen_fnal+=( --ghep-rock-name "$ARCUBE_GHEP_ROCK_NAME" )
+[ -n "${ARCUBE_MINERVA_NAME}" ] && args_gevgen_fnal+=( --minerva-name "$ARCUBE_MINERVA_NAME" )
+[ -n "${ARCUBE_TMSRECO_NAME}" ] && args_gevgen_fnal+=( --tmsreco-name "$ARCUBE_TMSRECO_NAME" )
+[ -n "${ARCUBE_HADD_FACTOR}" ] && args_gevgen_fnal+=( --hadd-factor "$ARCUBE_HADD_FACTOR" )
+
+./gen_cafmaker_cfg.py "${args_gen_cafmaker_cfg[@]}"
 
 echo ===================
 cat "$cfgFile"
