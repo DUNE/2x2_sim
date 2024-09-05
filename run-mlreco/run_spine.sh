76f0b78dce9c721fa8cbca7432102fc18e96834d
Alexander Booth <abooth@login06.chn.perlmutter.nersc.gov>
2024-09-05 10:16:08 -0700

    Make the spine config configurable by env variable and add a couple of (non-compulsory) n thread related variables needed for running spine on ndlar (Kazu's idea).

--- run-mlreco/run_spine.sh
+++ run-mlreco/run_spine.sh
@@ -5,10 +5,17 @@ source ../util/init.inc.sh
 
 source load_mlreco.inc.sh
 
+[ -z "$ARCUBE_SPINE_CONFIG" ] && export ARCUBE_SPINE_CONFIG="2x2_full_chain_240819.cfg"
+
+# Only export onwards if the vars are filled. The exports are a tip from Kazu and
+# required for NDLAr.
+[ -n "$ARCUBE_SPINE_NUM_THREADS" ] && export NUM_THREADS=$ARCUBE_SPINE_NUM_THREADS
+[ -n "$ARCUBE_SPINE_OPENBLAS_NUM_THREADS" ] && export OPENBLAS_NUM_THREADS=$ARCUBE_SPINE_OPENBLAS_NUM_THREADS
+
 outFile=${tmpOutDir}/${outName}.MLRECO_ANALYSIS.hdf5
 inName=${ARCUBE_IN_NAME}.${globalIdx}
 inFile=${ARCUBE_OUTDIR_BASE}/run-mlreco/${ARCUBE_IN_NAME}/LARCV/${subDir}/${inName}.LARCV.root
-config=2x2_full_chain_240819.cfg
+config=$ARCUBE_SPINE_CONFIG
 
 tmpDir=$(mktemp -d)
 mkdir "${tmpDir}/log_trash" 
