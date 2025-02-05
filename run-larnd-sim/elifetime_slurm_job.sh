78f44243e9ab9d7f8cc09e4ef96169083255e50f
Elise Dianne Hinkle <ehinkle@login40.chn.perlmutter.nersc.gov>
2025-02-04 19:01:26 -0800

    Adding infrastructure to run sbatch jobs on NERSC to run larnd-sim over manually constructed edep-sim-like hdf5 files.

---
+++ run-larnd-sim/elifetime_slurm_job.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#SBATCH --account=dune_g
+#SBATCH --qos=regular
+#SBATCH --constraint=gpu
+#SBATCH --time=2:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=32
+
+srun ./elifetime_slurm_task.py fake_edepsim_inputs_TEST.txt 
