08c5b2972b2993dfc99323df18c7859779412c3e
Matt Kramer <mjkramer@users.noreply.github.com>
2025-04-02 14:33:06 -0700

    Bump CUDA to 12.4

--- util/prelude.inc.sh
+++ util/prelude.inc.sh
@@ -6,6 +6,6 @@ set -o pipefail
 setup_cuda() {
     if [[ "$LMOD_SYSTEM_NAME" == "perlmutter" ]]; then
         module load python/3.11
-        module load cudatoolkit/12.2
+        module load cudatoolkit/12.4
     fi
 }
