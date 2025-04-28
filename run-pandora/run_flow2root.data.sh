c4f4c13264ec055be756780aabbedb2ac29e70a6
Matt Kramer <mjkramer@users.noreply.github.com>
2025-04-28 08:20:20 -0700

    Add ARCUBE_USE_FINAL_HITS

--- run-pandora/run_flow2root.data.sh
+++ run-pandora/run_flow2root.data.sh
@@ -24,7 +24,7 @@ inFile=${ARCUBE_FLOW_DIR_BASE}/${relDir}/${inName}
 rm -f "$outFile"
 
 isData=1
-isFinal=1
+isFinal=${ARCUBE_USE_FINAL_HITS:-1}
 
 source $ARCUBE_PANDORA_INSTALL/pandora.venv/bin/activate
 run python3 $ARCUBE_PANDORA_INSTALL/LArRecoND/ndlarflow/h5_to_root_ndlarflow.py $inFile $isData $isFinal ${outFile}.firstStep.root
