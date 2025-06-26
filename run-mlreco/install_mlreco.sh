63731158283b891a61a6407c2d1b1b67140a0c4a
Matt Kramer <mjkramer@users.noreply.github.com>
2025-06-25 20:14:51 -0700

    Bump SPINE

--- run-mlreco/install_mlreco.sh
+++ run-mlreco/install_mlreco.sh
@@ -14,12 +14,12 @@ set -o errexit
 # recommended by Jeremy
 unset which
 
-mkdir weights
+mkdir -p weights
 cd weights
 wget https://portal.nersc.gov/project/dune/data/2x2/simulation/mlreco_weights/2x2_240819_snapshot.ckpt
 cd ..
 
-mkdir install
+mkdir -p install
 cd install
 
 # The Ubuntu container doesn't support Python's built-in "venv" module unless we
@@ -66,7 +66,7 @@ pip install .
 cd ..
 
 
-git clone -b v4.1.9 https://github.com/DeepLearnPhysics/flow2supera.git
+git clone -b mjkramer/feature/trigger_times https://github.com/DeepLearnPhysics/flow2supera.git
 ## Don't pip install because e.g. config files are expected to live near
 ## __file__
 # cd flow2supera
@@ -92,7 +92,7 @@ cd ..
 # git clone -b jw_dune_nd_lar https://github.com/chenel/lartpc_mlreco3d.git
 
 #git clone -b v2.9.5 https://github.com/DeepLearnPhysics/lartpc_mlreco3d.git
-git clone -b v0.3.3 https://github.com/DeepLearnPhysics/spine.git
+git clone -b spine https://github.com/DeepLearnPhysics/spine.git
 
 # git clone https://github.com/chenel/dune-nd-lar-reco.git
 # the old yaml.load API has been removed
